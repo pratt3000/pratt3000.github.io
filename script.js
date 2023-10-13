db4f48453be75fdd47bf6d3547f4b91d2d4aa001
pratt3000 <pratt3000@gmail.com>
2023-10-13 16:11:56 -0500

    changed buttons to tabs

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 function showSection(sectionId) {
     // Hide all sections
-    var sections = document.getElementsByClassName('content-section');
-    for (var i = 0; i < sections.length; i++) {
-        sections[i].style.display = 'none';
-    }
-    
-    // Show the selected section
-    document.getElementById(sectionId).style.display = 'block';
+    $('.content-section').hide();
+
+    // Remove 'active' class from all buttons
+    $('.tab-button').removeClass('active');
+
+    // Show the specific section
+    $('#' + sectionId).show();
+
+    // Add 'active' class to the button that's been clicked
+    $('button[onclick="showSection(\'' + sectionId + '\')"]').addClass('active');
 }
+
+// A jQuery ready function to set the initial state of your page
+$(document).ready(function(){
+    // Assuming you want the first tab content to be displayed initially
+    $('.content-section').hide(); // Hide all content sections
+    $('#timeline').show(); // Show the first tab content
+    $('.tab-button').first().addClass('active'); // Set the first tab button as active
+});
+
 
 $(function () {
     $("#timeline").load("timeline.html");
