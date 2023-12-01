fab3c90269c6b768c39df2658b500bb359439a79
pratt3000 <pratt3000@gmail.com>
2023-12-01 16:06:53 -0600

    chore: fix small things

--- script.js
+++ script.js
@@ -1,25 +1,15 @@
 function showSection(sectionId) {
-    // Hide all sections
     $('.content-section').hide();
-
-    // Remove 'active' class from all buttons
     $('.tab-button').removeClass('active');
-
-    // Show the specific section
     $('#' + sectionId).show();
-
-    // Add 'active' class to the button that's been clicked
     $('button[onclick="showSection(\'' + sectionId + '\')"]').addClass('active');
 }
 
-// A jQuery ready function to set the initial state of your page
 $(document).ready(function(){
-    // Assuming you want the first tab content to be displayed initially
-    $('.content-section').hide(); // Hide all content sections
-    $('#timeline').show(); // Show the first tab content
-    $('.tab-button').first().addClass('active'); // Set the first tab button as active
+    $('.content-section').hide();
+    $('#timeline').show();
+    $('.tab-button').first().addClass('active');
 });
-
 
 $(function () {
     $("#timeline").load("timeline.html");
