658177b65b03996077761d0742f217ae708668fc
pratt3000 <pratt3000@gmail.com>
2026-04-02 12:52:18 -0700

    made the navigation sticky

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ function showSection(sectionId) {
     $('.tab-button').removeClass('active');
     $('#' + sectionId).show();
     $('button[onclick="showSection(\'' + sectionId + '\')"]').addClass('active');
+    $('#mobile-nav-select').val(sectionId);
 }
 
-$(document).ready(function(){
+$(document).ready(function () {
     $('.content-section').hide();
     $('#timeline').show();
     $('.tab-button').first().addClass('active');
@@ -17,4 +18,13 @@ $(function () {
     $("#work-experience").load("workex.html");
     $("#education").load("education.html");
     $("#misc").load("misc.html");
+});
+
+$(window).on('scroll', function () {
+    var menu = document.querySelector('.menu');
+    if (menu && menu.getBoundingClientRect().top <= 1) {
+        menu.classList.add('is-sticky');
+    } else {
+        menu.classList.remove('is-sticky');
+    }
 });
